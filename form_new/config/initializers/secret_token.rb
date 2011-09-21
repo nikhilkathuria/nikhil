7fbad9813b11463386337fa76f56ec92aaa101ef
nikhilkathuria <nikhil@192.168.3.99>
2011-09-21 17:04:24 +0530

    second commit

---
+++ form_new/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormNew::Application.config.secret_token = '9b3b96da5b09bf9856d0328d8249aa79067c5ca35e3a3e51cb8f454c6f47397e96a71da0b04b8297f7154323d64baae9ab6464483569a64f1ce5bb5282f480e0'
